5981234315406beb72455eacdc27604a0df5dbbb
EmilieGonin <emilie.yuu@gmail.com>
2022-12-06 09:37:40 +0100

    add comment about creating files

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// Lors de l'ajout d'un fichier .h, modifier l'emplacement du fichier vers le dossier "include"
+// Lors de l'ajout d'un fichier .cpp, modifier l'emplacement du fichier vers le dossier "src
+
 #include <iostream>
 
 int main()
