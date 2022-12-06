eec5095a46cff4cbca5f275a96386670b36c856f
EmilieGonin <emilie.yuu@gmail.com>
2022-12-06 17:38:43 +0100

    add comment

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // Lors de l'ajout d'un fichier .cpp, modifier l'emplacement du fichier vers le dossier "src
 
 #include <iostream>
-#include "functions.h"
+#include "functions.h" //Temporary functions outside classes
 
 int main()
 {
