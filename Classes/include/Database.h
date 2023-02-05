3835ac3d84d58b61cded9e346ac404e1c60218b4
EmilieGonin <emilie.yuu@gmail.com>
2023-02-05 17:16:26 +0100

    add requests to create or get user depending on local data check

--- Classes/include/Database.h
+++ Classes/include/Database.h
@@ -3,24 +3,31 @@
 #include <cpr/cpr.h>
 #include "cocos2d.h"
 #include <nlohmann/json.hpp>
+#include <fstream>
+#include <string>
 
 class Database
 {
 protected:
 	std::string _url; //Testing only - to encrypt
+	cpr::Response _request;
+	std::string _email;
 
 public:
 	Database();
 
 	//Wrapper pour créer les requêtes et gérer les erreurs
 	//Peut être utilisé pour les requêtes GET et POST avec une surcharge
-	cpr::Response request(std::string); //GET
-	cpr::Response request(std::string, nlohmann::json); //POST
-	void handleRequest(cpr::Response);
+	bool request(std::string); //GET
+	bool request(std::string, nlohmann::json); //POST
+	bool handleRequest(cpr::Response);
+	std::vector<std::string> split(std::string, std::string);
+	bool checkSave();
+
 
 	//GET requests
-	void getUser();
+	bool getUser();
 
 	//POST requests
-	void createUser();
+	bool createUser();
 };
--- Classes/src/Database.cpp
+++ Classes/src/Database.cpp
@@ -12,50 +12,102 @@ namespace db { //Les structures et fonctions utilisées pour le JSON
 }
 
 Database::Database() {
-	_url = "https://49g5s1t0.directus.app";
-	createUser();
+	_url = "https://49g5s1t0.directus.app"; //To encrypt
+
+	if (!checkSave()) {
+		if (!createUser()) { //À remplacer par un while avec saisie utilisateur
+			cocos2d::log("User already exist !");
+		}
+	}
+	else {
+		getUser();
+	}
 }
 
-cpr::Response Database::request(std::string url) {
-	cpr::Response r = cpr::Get(cpr::Url{ url }, cpr::VerifySsl{ false });
-	handleRequest(r);
-	return r;
+bool Database::checkSave() {
+	bool dataFound = false;
+	std::ifstream file("user.txt");
+
+	if (file.is_open()) {
+		cocos2d::log("file opened");
+		std::string line;
+		do {
+			if (!line.empty()) {
+				std::vector<std::string> data = split(line, "=");
+
+				if (data[0] == "User") {
+					_email = data[1];
+					cocos2d::log("user data found");
+					dataFound = true;
+				}
+			}
+		} while (std::getline(file, line));
+	}
+
+	file.close();
+	return dataFound;
 }
 
-cpr::Response Database::request(std::string url, json payload) {
-	cpr::Response r = cpr::Post(
+std::vector<std::string> Database::split(std::string string, std::string delim) {
+	std::size_t delimIndex = string.find(delim);
+	std::string setting = string.substr(0, delimIndex);
+	std::string value = string.substr(delimIndex + 1);
+
+	std::vector<std::string> data = { setting, value };
+	return data;
+}
+
+bool Database::request(std::string url) {
+	_request = cpr::Get(cpr::Url{ url }, cpr::VerifySsl{ false });
+	return handleRequest(_request);
+}
+
+bool Database::request(std::string url, json payload) {
+	_request = cpr::Post(
 		cpr::Url{ url }, cpr::VerifySsl{ false },
 		cpr::Body{ payload.dump() },
 		cpr::Header{ {"Content-Type", "application/json"} });
-	handleRequest(r);
-	return r;
+
+	return handleRequest(_request);
 }
 
-void Database::handleRequest(cpr::Response r) {
+bool Database::handleRequest(cpr::Response r) {
 	cocos2d::log("**********"); //Help to see logs
 
 	if (r.status_code == 0) { //Si la requête n'a pas pu être lancée
 		cocos2d::log(r.error.message.c_str());
+		return false;
 	}
 	else if (r.status_code >= 400) { //Si la requête a échoué
 		std::string message = std::string("Error [" + std::to_string(r.status_code) + "] making request.");
 		cocos2d::log(message.c_str());
+		return false;
 	}
 	else { //On affiche le résultat dans la console (debug only)
 		cocos2d::log(r.text.c_str());
+		return true;
 	}
 
 	cocos2d::log("**********"); //Help to see logs
 }
 
-void Database::getUser() {
-	std::string url = std::string(_url + "/items/users");
-	cpr::Response r = request(url);
+bool Database::getUser() {
+	cocos2d::log("getting user");
+	std::string url = std::string(_url + "/items/users?filter[mail][_eq]=" + _email);
+	return request(url);
 }
 
-void Database::createUser() {
+bool Database::createUser() {
+	cocos2d::log("creating user");
+	_email = "test@test.com";
+
+	//Creating user
+	std::ofstream file("user.txt");
+	file << "User=" + _email << std::endl;
+	file.close();
+
 	std::string url = std::string(_url + "/items/users");
-	db::user user = { std::string("test@test.com") }; //Création de la struct
+	db::user user = { std::string(_email) }; //Création de la struct
 	json payload = user; //On convertis la struct en JSON
-	cpr::Response r = request(url, payload);
+	return request(url, payload);
 }
