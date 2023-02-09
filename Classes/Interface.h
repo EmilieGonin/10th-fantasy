ce89e92a56b192ee200457b132c2662245be3dff
LeannF <leann.farant@outlook.fr>
2023-02-09 18:06:40 +0100

    test

--- Classes/Interface.h
+++ Classes/Interface.h
@@ -3,9 +3,32 @@
 #include "ui/CocosGUI.h"
 
 using namespace cocos2d::ui;
+using namespace cocos2d;
 
-TextField* newTextField(std::string);
-Button* newButton(std::string string);
-cocos2d::Vec2 center();
-float centerWidth();
-float centerHeight();
+class Interface {
+protected:
+	Scene* _scene;
+	//On stocke les différents éléments d'interface à afficher
+	std::vector<TextField*> _textFields;
+	std::vector<Button*> _buttons;
+	std::vector<Label*> _labels;
+
+public:
+	Interface();
+
+	//Création d'éléments automatiquement ajoutés dans la scène définie
+	TextField* newTextField(std::string);
+	Button* newButton(std::string);
+	Label* newLabel(std::string);
+
+	//Des wrappers pour set les positions
+	Vec2 center();
+	float centerWidth();
+	float centerHeight();
+
+	//On retire tous les éléments d'interface créés
+	void clean();
+
+	//Setters
+	void setScene(Scene*);
+};
